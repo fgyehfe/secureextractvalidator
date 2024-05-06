b0c9a066eaeb91e43a25190c622e04825239f928
fgyehfe <fsjcamilleroser@hotmail.com>
2024-05-06 08:42:41 +0700

    demo task

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+function isSymmetric(root) {
+  if (!root) return true;
+  return isMirror(root.left, root.right);
+  function isMirror(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+  }
+}
